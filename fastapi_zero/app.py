86e89926df01ddb733c94d37e862c7be46185cdb
miguelsett <miguelsettimio@gmail.com>
2025-06-01 19:41:44 -0300

    Criando tabela de usuários

--- fastapi_zero/app.py
+++ fastapi_zero/app.py
@@ -12,7 +12,7 @@ from fastapi_zero.schemas import (
 
 app = FastAPI()
 
-database = []  # Lista provisória para fins de estudo
+database = []
 
 
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
@@ -57,13 +57,3 @@ def delete_user(user_id: int):
     del database[user_id - 1]
 
     return {'message': 'User deleted'}
-
-
-@app.get('/users/{user_id}', response_model=UserPublic)
-def read_user__exercicio(user_id: int):
-    if user_id > len(database) or user_id < 1:
-        raise HTTPException(
-            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
-        )
-
-    return database[user_id - 1]
